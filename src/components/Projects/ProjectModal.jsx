b90e6df14aba40ce343c515850cb7a5bd641c92f
IsmailMainal <ismailmanyal3@gmail.com>
2026-07-03 22:52:39 +0530

    Fix scroll blocking inside ProjectModal with data-lenis-prevent

--- src/components/Projects/ProjectModal.jsx
+++ src/components/Projects/ProjectModal.jsx
@@ -41,6 +41,7 @@ export default function ProjectModal({ project, onClose }) {
 
         {/* Modal Window Container */}
         <motion.div
+          data-lenis-prevent
           initial={{ opacity: 0, scale: 0.95, y: 20 }}
           animate={{ opacity: 1, scale: 1, y: 0 }}
           exit={{ opacity: 0, scale: 0.95, y: 20 }}
